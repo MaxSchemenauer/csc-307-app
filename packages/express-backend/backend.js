4eb9bafd49a3bf68ba3268c8730d81cf7ac52f5c
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2024-04-15 16:48:54 -0700

    gets users by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,12 +6,63 @@ const port = 8000;
 
 app.use(express.json());
 
+
+const users = {
+    users_list: [
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]
+};
+
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
+});
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("did you mean to go to http://localhost:8000/users?name=[name]");
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+    console.log(
+        `Example app listening at http://localhost:${port}`
+    );
 });
+
